50e6297493876a670a3a25aba104930d3cee3658
Sakura <sakurathetrans@gmail.com>
2025-01-23 12:52:14 -0500

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        String password = "orb";
+        System.out.println("Guess password : ");
+        String guess = input.nextLine();
+        if (guess.equals(password)){
+            System.out.println("CORRECT !!!");
+        } else if(!(guess.equals(password))) {
+            System.out.println("WRONG !!!");
+        }
     }
 }
